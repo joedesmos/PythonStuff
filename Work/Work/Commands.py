98867a84b5edd019e022d37f9be242e85ad0cddc
joe <youremail@yourdomain.com>
2026-06-22 09:26:36 -0400

    Teaching Python

---
+++ Work/Work/Commands.py
@@ -0,0 +1,25 @@
+#Print Statements
+print("Hello World")
+
+#Comments
+#Single Line
+
+"""
+Multi Line
+Comment
+"""
+
+#Variables
+x = 5
+y = "Hello"
+
+print(x)
+print(f"Hello my name is Ryland")
+
+#Values
+a = "Hello" #String
+b = 5 #Integer
+c = 5.10 #Float
+d = True #Boolean
+
+print(type(a))
