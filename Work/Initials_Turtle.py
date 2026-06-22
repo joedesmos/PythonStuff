c763d4ecf3b7e36571f6329554ec6ceeb389470f
joedesmos <rydesantis44@gmail.com>
2026-06-22 14:15:56 -0400

    Fun with turtle

---
+++ Work/Initials_Turtle.py
@@ -0,0 +1,44 @@
+from turtle import *
+
+#Get into position (Came back later and realised I could have used goto)
+penup()
+backward(200)
+left(90)
+forward(150)
+pendown()
+
+#R
+color("green")
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(180)
+forward(200)
+right(180)
+forward(100)
+right(135)
+forward(125)
+left(180)
+forward(125)
+right(45)
+forward(100)
+right(90)
+penup()
+forward(200)
+pendown()
+right(90)
+
+#D
+color("blue")
+forward(200)
+right(180)
+right(90)
+circle(100, 180)
+
+#Finish
+done()
