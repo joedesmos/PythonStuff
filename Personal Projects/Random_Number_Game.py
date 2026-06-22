bf71e050ccc45db989c31c024e9edd8ee715a0e4
joedesmos <rydesantis44@gmail.com>
2026-06-22 10:22:33 -0400

    Moved random number game to Personal Projects

--- Personal Projects/Random_Number_Game.py
+++ Personal Projects/Random_Number_Game.py
@@ -1,17 +1,22 @@
 import random
 
-# Generate random number between 1 and 100
 random_number = random.randint(1, 100)
+guesses = 0
+guess = None
 
-#Attempts counter
-attempts = 0
-
-#Game loop
-guess = int(input("Guess a Number: "))
 while guess != random_number:
-    if guess < random_number:
-        print("Too low!")
-    else:
-        print("Too high!")
-    guess = int(input("Guess a Number: "))
-print("Congrats! You guessed the number!")
+    try:
+        guess = int(input("Guess a number 1-100: "))
+        guesses += 1
+
+        if guess > random_number:
+            print("Too High")
+
+        elif guess < random_number:
+            print("Too Low")
+
+        else:
+            print(f"You guessed the number in {guesses} attempts.")
+
+    except ValueError:
+        print("Please type a number")
